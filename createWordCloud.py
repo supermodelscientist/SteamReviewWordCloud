7933a720eb2e15de80b6f13029cc966d384ca04f
supermodelscientist <31076322+supermodelscientist@users.noreply.github.com>
2022-02-12 17:38:34 -0500

    improved review get

--- createWordCloud.py
+++ createWordCloud.py
@@ -5,8 +5,9 @@ from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
 
-reviewList = grabReviewJSON.getList()
+reviews = grabReviewJSON.get_n_reviews('1454400',5000)
+reviewList = [x['review'] for x in reviews.json()]
 collapsedList = ' '.join(reviewList)
 STOPWORDS.update(['game','cookie','cookies','clicker','play','idle'])
-wordcloud = WordCloud().generate(text=collapsedList)
-wordcloud.to_file('first_100_neg.png')
+wordcloud = WordCloud(collocation_threshold = 3, width=1600, height=800).generate(text=collapsedList)
+wordcloud.to_file('first_100.png')
--- grabReviewJSON.py
+++ grabReviewJSON.py
@@ -1,7 +1,34 @@
 import requests
-payload = {'filter':'recent', 'review_type':'negative','num_per_page':'100'}
-response = requests.get("https://store.steampowered.com/appreviews/1454400?json=1",payload)
+import time
 
-responseJSON = response.json()
-reviews = responseJSON['reviews']
-reviewList = [x['review'] for x in reviews]
+
+def get_reviews(appid, params={'json': 1}):
+    url = 'https://store.steampowered.com/appreviews/'
+    response = requests.get(url=url + appid, params=params, headers={'User-Agent': 'Mozilla/5.0'})
+    return response.json()
+
+def get_n_reviews(appid, n=100):
+    reviews = []
+    cursor = '*'
+    params = {
+            'json' : 1,
+            'filter' : 'all',
+            'language' : 'english',
+            'day_range' : 9223372036854775807,
+            'review_type' : 'all',
+            'purchase_type' : 'all'
+            }
+
+    while n > 0:
+        params['cursor'] = cursor.encode()
+        params['num_per_page'] = min(100, n)
+        n -= 100
+
+        response = get_reviews(appid, params)
+        cursor = response['cursor']
+        reviews += response['reviews']
+
+        if len(response['reviews']) < 100: break
+
+    return reviews
+
