2b8f8021061f1f69c5dbe5bf020a0e185e5b8209
supermodelscientist <31076322+supermodelscientist@users.noreply.github.com>
2022-02-12 18:05:28 -0500

    cleanup

--- createWordCloud.py
+++ createWordCloud.py
@@ -1,10 +1,9 @@
 import grabReviewJSON
 from wordcloud import WordCloud, STOPWORDS
 
-reviews = grabReviewJSON.get_n_reviews('427520',2000)
+reviews = grabReviewJSON.get_n_reviews('814540',2000)
 reviewList = [x['review'] for x in reviews]
 collapsedList = ' '.join(reviewList)
-STOPWORDS.update(['hr','b','h1','o','u'])
+STOPWORDS.update(['hr','b','h1','o','u', 'game'])
 wordcloud = WordCloud(collocation_threshold = 3, width=1600, height=800).generate(text=collapsedList)
-wordcloud.to_file('first_100.png')
-
+wordcloud.to_file('wordcloud.png')
