185766d7f9cda2d899250c5d4f6cb29f0be0ff8e
supermodelscientist <31076322+supermodelscientist@users.noreply.github.com>
2022-02-12 17:50:37 -0500

    fucking around

--- createWordCloud.py
+++ createWordCloud.py
@@ -1,13 +1,10 @@
 import grabReviewJSON
-import numpy as np
-import pandas as pd
-from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
 
-reviews = grabReviewJSON.get_n_reviews('1454400',5000)
-reviewList = [x['review'] for x in reviews.json()]
+reviews = grabReviewJSON.get_n_reviews('427520',2000)
+reviewList = [x['review'] for x in reviews]
 collapsedList = ' '.join(reviewList)
-STOPWORDS.update(['game','cookie','cookies','clicker','play','idle'])
+STOPWORDS.update(['hr','b','h1','o','u'])
 wordcloud = WordCloud(collocation_threshold = 3, width=1600, height=800).generate(text=collapsedList)
 wordcloud.to_file('first_100.png')
+
